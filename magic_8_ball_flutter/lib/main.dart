06e7d219fd445d78db7d2180471bd3972ffe0fb4
unknown <jiale98chinoguay@gmail.com>
2024-10-24 22:50:08 -0400

    initialize layout of AppBar and Body

--- magic_8_ball_flutter/lib/main.dart
+++ magic_8_ball_flutter/lib/main.dart
@@ -2,6 +2,45 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.black87,
+            title: Text('Ask me anything', style: TextStyle(color: Colors.white70)),
+            centerTitle: true,
+          ),
+          body: MainBodyPage(),
+        ),
       ),
     );
+
+class MainBodyPage extends StatefulWidget {
+  const MainBodyPage({super.key});
+
+  @override
+  State<MainBodyPage> createState() => _MainBodyPageState();
+}
+
+class _MainBodyPageState extends State<MainBodyPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        color: Colors.black45,
+        child: Expanded(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 80),
+                child: Image.asset(
+                  'images/ball1.png',
+                  fit: BoxFit.cover
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
