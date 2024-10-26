bdba7a8e17345bac448bf7ade7e512fa01f18ed0
unknown <jiale98chinoguay@gmail.com>
2024-10-26 13:41:22 -0400

    [Add]: assets_audio_player library to play audio files

--- xylophone_flutter/lib/main.dart
+++ xylophone_flutter/lib/main.dart
@@ -1,14 +1,29 @@
+import 'package:assets_audio_player/assets_audio_player.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
+  playAudio(audioFileName) {
+    AssetsAudioPlayer.newPlayer().open(
+        Audio(audioFileName),
+        autoStart: true,
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: TextButton(
+              onPressed: () {
+                playAudio("note2.wav");
+              },
+              child: Text('button')
+            )
+          ),
         ),
       ),
     );
