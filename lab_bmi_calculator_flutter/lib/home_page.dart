9f12509c179c584f8b9594c38dc3b8969a8dfd54
unknown <jiale98chinoguay@gmail.com>
2024-11-19 22:33:13 -0500

    refactor code for reusable card

--- lab_bmi_calculator_flutter/lib/home_page.dart
+++ lab_bmi_calculator_flutter/lib/home_page.dart
@@ -10,7 +10,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +19,7 @@ class _HomePageState extends State<HomePage> {
       ),
       // body: const Column(
       //   children: [
-          
+
       //   ],
       // )
       body: Column(
@@ -29,61 +28,50 @@ class _HomePageState extends State<HomePage> {
             child: Row(
               children: [
                 Expanded(
-                  child: Container(
-                    margin: const EdgeInsets.all(15),
-                    decoration: BoxDecoration(
-                      color: const Color(0xFF1D1E33),
-                      borderRadius: BorderRadius.circular(10)
-                    ),
-                  ),
+                  child: ReusableCard(color: const Color(0xFF1D1E33))
                 ),
                 Expanded(
-                  child: Container(
-                    margin: const EdgeInsets.all(15),
-                    decoration: BoxDecoration(
-                      color: const Color(0xFF1D1E33),
-                      borderRadius: BorderRadius.circular(10)
-                    ),
-                  ),
+                  child: ReusableCard(color: const Color(0xFF1D1E33))
                 ),
               ],
             ),
           ),
           Expanded(
-            child: Container(
-              margin: const EdgeInsets.all(15),
-              decoration: BoxDecoration(
-                color: const Color(0xFF1D1E33),
-                borderRadius: BorderRadius.circular(10)
-              ),
-            ),
+            child: ReusableCard(color: const Color(0xFF1D1E33)),
           ),
           Expanded(
             child: Row(
               children: [
                 Expanded(
-                  child: Container(
-                    margin: const EdgeInsets.all(15),
-                    decoration: BoxDecoration(
-                      color: const Color(0xFF1D1E33),
-                      borderRadius: BorderRadius.circular(10)
-                    ),
-                  ),
+                  child: ReusableCard(color: const Color(0xFF1D1E33))
                 ),
                 Expanded(
-                  child: Container(
-                    margin: const EdgeInsets.all(15),
-                    decoration: BoxDecoration(
-                      color: const Color(0xFF1D1E33),
-                      borderRadius: BorderRadius.circular(10)
-                    ),
-                  ),
+                  child: ReusableCard(color: const Color(0xFF1D1E33))
                 ),
               ],
             ),
           ),
         ],
       ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({
+    super.key, 
+    required this.color
+  });
+
+  Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(15),
+      decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(10)),
     );
   }
 }
