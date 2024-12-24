cf5316c862158059fe94192c90b2a96cc29d6b1f
unknown <jiale98chinoguay@gmail.com>
2024-12-23 20:07:28 -0500

    update

--- 06_lab_bmi_calculator_flutter/lib/main.dart
+++ 06_lab_bmi_calculator_flutter/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:lab_bmi_calculator_flutter/home_page.dart';
+import 'package:lab_bmi_calculator_flutter/screens/home_page.dart';
 
 void main() {
   runApp(const BmiCalculator());
